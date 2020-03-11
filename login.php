a945e586e7e05873893aab3058101bc951474b97
diy16102 <60899705+diy16102@users.noreply.github.com>
2020-03-11 02:07:28 -0400

    Add files via upload

--- login.php
+++ login.php
@@ -1,46 +1,41 @@
+<!DOCTYPE html>
+<html><head>
+<title>login</title>
+<meta name="content-type"; charset="UTF-8">
+</head><body> 
+<div class="content" align="center"> 
+ <div class="header"> <h1>Login</h1> </div> 
+<!--中部--> 
+<div class="middle">
+ <form id="loginform" action="loginaction.php" method="post"> 
+<table border="0"> <tr> 
+    <td>Username：</td> 
+<td> <input type="text" id="name" name="username" 
+required="required" value="<?php
+echo isset($_COOKIE[""]) ? $_COOKIE[""] : ""; ?>"> </td> </tr> 
+<tr> <td>Password：</td> <td><input type="password" id="password" name="password"></td> 
+</tr> <tr> <td colspan="2"> <input type="checkbox" name="remember"><small>Remember me </td> </tr> <tr> <td 
+colspan="2" align="center" style="color:red;font-size:10px;">
 <?php
-	header("Content-Type: text/html;charset=utf-8");
-	//create connect
-	$conn = mysqli_connect('localhost','root','root');
-	if($conn){
-		//database connect success
-		$select = mysqli_select_db($conn,"user_login");		//select database
-		if($select){
-			//success
-			if(isset($_POST["subl"])){
-				
-				$user = $_POST["username"];
-				$pass = $_POST["password"];
-				if($user == ""||$pass == ""){
-					//empty
-					//backtologin
-					echo"<script type="."\""."text/javascript"."\"".">"."window.alert"."("."\""."Cannot be empty"."\"".")".";"."</script>";
-					echo"<script type="."\""."text/javascript"."\"".">"."window.location="."\""."login.html"."\""."</script>";
-					exit;
-				}
-				//sql
-				$sql_select = "select username,password from users where username = '$user' and password = '$pass'";
-				//
-				mysqli_query($conn,'SET NAMES UTF8');
-				//
-				$ret = mysqli_query($conn,$sql_select);
-				$row = mysqli_fetch_array($ret); 
-				
-				//correct
-				if($user == $row['username']&&$pass == $row['password']){
-					//jump to success
-					 header("Location:loginsucc.php");
-				}else{
-					//jump to fail
-					header("Location:loginfal.php");
-				}  
-			}
-		}
-		//close
-		mysqli_close($conn);
-	}else{		
-		//error
-		die('Could not connect:'.mysql_error());
-	}	
- 
-?>
+$err = isset($_GET["err"]) ? $_GET["err"] : "";
+switch ($err) {
+    case 1:
+        echo "Username or Password is wrong";
+        break;
+
+    case 2:
+        echo "Usesrname and Password cannot be empty！";
+        break;
+} ?> </td> </tr> <tr> <td colspan="2" align="center"> 
+<input type="submit" id="login" name="login" value="login"> <input type="reset" id="reset" 
+name="reset" value="reset"> </td> </tr> 
+<tr> 
+    <td colspan="2" align="center"> Have no account yet,<a href="register.php">Register</a></td>
+</tr> 
+</table> 
+</form> 
+</div> 
+
+<div class="footer"> <small>Copyright &copy; </div> </div>
+</body>
+</html>     
--- register.php
+++ register.php
@@ -1,59 +1,45 @@
-<?php
-	header("Content-Type: text/html;charset=utf-8");
-	//connect
-	$conn = mysqli_connect('localhost','root','root');
-	if($conn){
-		//success
-		$select = mysqli_select_db($conn,"user_login");		//select
-		if(isset($_POST["subr"])){
-			
-			$user = $_POST["username"];
-			$pass = $_POST["password"];
-			$re_pass = $_POST["re_password"];
-			
-			if($user == ""||$pass == ""){
-				
-				echo"<script type="."\""."text/javascript"."\"".">"."window.alert"."("."\""."Username and passwords cannot be empty！"."\"".")".";"."</script>";
-				echo"<script type="."\""."text/javascript"."\"".">"."window.location="."\""."register.html"."\""."</script>";
-				exit;
-			}
-			if($pass == $re_pass){
-				
-				mysqli_set_charset($conn,'utf8');	
-				
-				//sql
-				$sql_select = "select username from users where username = '$user'";
-				
-				$result = mysqli_query($conn,$sql_select);
-				
-				$num = mysqli_num_rows($result); 
-				
-				if($num){
-					
-					echo"<script type="."\""."text/javascript"."\"".">"."window.alert"."("."\""."Usename exist！"."\"".")".";"."</script>";
-					echo"<script type="."\""."text/javascript"."\"".">"."window.location="."\""."register.html"."\""."</script>";
-					exit;
-				}else{
-					
-					$sql_insert = "insert into users(username,password) values('$user','$pass')";
-					
-					$ret = mysqli_query($conn,$sql_insert);
-					$row = mysqli_fetch_array($ret); 
-					
-					header("Location:registersucc.php");
-				}
-			}else{
-				
-				echo"<script type="."\""."text/javascript"."\"".">"."window.alert"."("."\""."password not same！"."\"".")".";"."</script>";
-				echo"<script type="."\""."text/javascript"."\"".">"."window.location="."\""."register.html"."\""."</script>";
-				exit;
-			}
-		}
-		
-		mysqli_close($conn);
-	}else{
-		
-		die('Could not connect:'.mysql_error());
-	}
+<!DOCTYPE html>
+<html>
+<head><title>Register</title>
+<meta name="content-type"; charset="UTF-8">
+</head><body> 
+<div class="content" align="center">  
+<div class="header"> <h1>Register</h1> </div> 
+<div class="middle"> 
+<form action="registeraction.php" method="post"> <table border="0"> 
+<tr> <td>Username：</td> 
+<td><input type="text" id="id_name" name="username" required="required"></td> 
+</tr> <tr>
+ <td>Password：</td> <td><input type="password" id="password" name="password" 
+required="required"></td> 
+</tr> <tr>
+ <td>Reprint password：</td> <td><input type="password" id="re_password" 
+name="re_password" required="required"></td> </tr> <tr>
+ <td>gender：</td> <td> <input type="radio" id="sex" name="sex" value="mam">Male <input type="radio" id="sex" name="sex" value="woman">Female </td> </tr> <tr>
  
-?>
+<td>Email：</td> <td><input type="email" id="email" name="email" required="required"></td> </tr> <tr> 
+<td>Phone number：</td> <td><input type="text" id="phone" name="phone" required="required"></td> </tr> <tr> 
+<td>Address：</td> <td><input type="text" id="address" name="address" required="required"></td> </tr> 
+<tr> <td colspan="2" align="center" style="color:red;font-size:10px;">  
+<?php
+$err = isset($_GET["err"]) ? $_GET["err"] : "";
+switch ($err) {
+    case 1:
+        echo "Username is already exist！";
+        break;
+
+    case 2:
+        echo "Passwords are not the same！";
+        break;
+
+    case 3:
+        echo "Success！";
+        break;
+}
+?> 
+</td> </tr> <tr> <td colspan="2" align="center"> 
+<input type="submit" id="register" name="register" value="Register">
+ <input type="reset" id="reset" name="reset" value="Reset"> </td></tr> 
+ <tr> <td colspan="2" align="center"> 
+If you have an account <a href="login.php">Login</a> </td> </tr> </table> </form> </div> 
+
